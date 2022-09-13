8b7f804dc13dcd0dc3b28d5025d4dc9d94d3dd5d
elnasrabi <elnasrabi@gmail.com>
2022-09-13 19:30:27 +1000

    Reorder Download Columns

--- backend/Flask.py
+++ backend/Flask.py
@@ -710,6 +710,6 @@ def get_ML_Current_exception():
 
 if __name__ == "__main__":
     print("Starting Python Flask Server")
-    CORS(__name__ )
+    #CORS(__name__ )
     app.run(port=5051 , ssl_context=('C:\cert\CA\localhost\cert.pem', 'C:\cert\CA\localhost\key.pem'))
     
